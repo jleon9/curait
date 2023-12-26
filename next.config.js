a07ec57729461fa1dc188e3539ef261690a265eb
jleon9 <jean-francois.leon@mail.mcgill.ca>
2023-12-26 16:36:00 +0000

    Might be dependency issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-// Example next.config.js for CSS modules
-module.exports = {
-    // Your other configurations...
-    cssModules: true,
-    webpack(config) {
-        console.log('Building with config:', config);
-        return config;
-      },
-  };
-  
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+  }
+   
+module.exports = nextConfig
