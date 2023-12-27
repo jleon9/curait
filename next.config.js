dae286aecaa19ebac85f8d5b248ae990c47073be
jleon9 <jean-francois.leon@mail.mcgill.ca>
2023-12-27 14:52:50 +0000

    Changed the playlist to conditionally render on playlistLink

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    /* config options here */
-    assetPrefix: 'https://embed-cdn.spotifycdn.com/'
   }
    
 module.exports = nextConfig
