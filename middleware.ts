088d303e1a04c9043736199a57297eef4427f5cd
jleon9 <jean-francois.leon@mail.mcgill.ca>
2025-05-16 12:50:48 +0200

    Fixing CORS issue with middleware

--- middleware.ts
+++ middleware.ts
@@ -5,27 +5,21 @@ import type { NextRequest } from 'next/server';
 export async function middleware(req: NextRequest) {
   const response = NextResponse.next();
 
-  // Check if the request is for an API route
-  if (req.nextUrl.pathname.startsWith('/api/')) {
-    // Set CORS headers
-    response.headers.append('Access-Control-Allow-Credentials', 'true');
-    response.headers.append(
-      'Access-Control-Allow-Origin',
-      'https://curait.vercel.app'
-    );
-    response.headers.append(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS'
-    );
-    response.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version'
-    );
-    response.headers.append(
-        'Permissions-Policy',
-        'encrypted-media=*'
-      );
-  }
+  // Set CORS headers
+  response.headers.append('Access-Control-Allow-Credentials', 'true');
+  response.headers.append(
+    'Access-Control-Allow-Origin',
+    '*'
+  );
+  response.headers.append(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  response.headers.append(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version'
+  );
+  response.headers.append('Permissions-Policy', 'encrypted-media=*');
 
   return response;
 }
