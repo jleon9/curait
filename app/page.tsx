5e44414a0e32f83fb861db86108c8eaecb667826
jleon9 <jean-francois.leon@mail.mcgill.ca>
2025-05-16 11:54:54 +0200

    Removed possibly problematic layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ export const metadata = {
   title: 'Home - Open PRO',
   description: 'Page description',
 };
-import PlaylistForm from '@/components/PlaylistForm';
+import PlaylistForm from '../components/PlaylistForm';
+import React from 'react';
 
 export default function Home() {
   return (
